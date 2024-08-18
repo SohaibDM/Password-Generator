1efa1c3660d7e15b601cd5d6f7f46ac6260abe54
sohaibDM <sohibdaami00@gmail.com>
2024-08-18 15:54:13 -0400

    created a chrome extension

--- index.js
+++ index.js
@@ -4,82 +4,89 @@ let includeUppercase = false;
 let includeNumbers = false;
 let includeSymbols = false;
 
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("generatebtn").addEventListener("click", generate);
+  document.getElementById("donebtn").addEventListener("click", handleClick);
+  document.getElementById("settingsIcon").addEventListener("click", toggleDivs);
+  document.getElementById("copybtn").addEventListener("click", copyText);
+});
 
+function generatePassword(
+  length,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+) {
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numberChars = "0123456789";
+  const symbolChars = "!@#$%^&*?_-()+=><";
 
+  let allowedChars = "";
+  let password = "";
+  let message = "";
 
-function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
-    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numberChars = '0123456789';
-    const symbolChars = '!@#$%^&*?_-()+=><';
+  allowedChars += includeLowercase ? lowercaseChars : "";
+  allowedChars += includeUppercase ? uppercaseChars : "";
+  allowedChars += includeNumbers ? numberChars : "";
+  allowedChars += includeSymbols ? symbolChars : "";
 
-    let allowedChars = "";
-    let password = "";
-    let message = "";
-
-    allowedChars += includeLowercase ? lowercaseChars: "";
-    allowedChars += includeUppercase ? uppercaseChars : "";
-    allowedChars += includeNumbers ? numberChars : "";
-    allowedChars += includeSymbols ? symbolChars : "";
-
-    
-
-    if (length > 20) {
-        message = "password cant be more than 20 chars";
-      document.getElementById("password").textContent = message;
+  if (length > 20) {
+    message = "password cant be more than 20 chars";
+    document.getElementById("password").textContent = message;
+  } else if (length <= 0) {
+    message = "Password length must be at least 1";
+    document.getElementById("password").textContent = message;
+  } else if (allowedChars.length === 0) {
+    message = "At least 1 set of characters needs to be selected";
+    document.getElementById("password").textContent = message;
+  } else {
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * allowedChars.length);
+      password += allowedChars[randomIndex];
     }
-    if(length <= 0){
-        message = ' Password length must be at least 1 ';
-        document.getElementById("password").textContent = message;
-    }
-
-    if(allowedChars.length === 0){
-        message = ' At least 1 set of characters needs to be selected ';
-        document.getElementById("password").textContent = message;
-    }
-
-    for ( let i = 0; i< length; i++){
-        const randomIndex = Math.floor(Math.random() * allowedChars.length);
-        password += allowedChars[randomIndex];
-    }
-    
-    
-    if (message === ""){
-        document.getElementById("password").textContent = password;
-        document.getElementById("copybtn").style.display = "block";
-    }
-    
+    document.getElementById("password").textContent = password;
+    document.getElementById("copybtn").style.display = "block";
+  }
 }
 
-function done(){
-    this.passwordLength = document.getElementById("length").value;
-    if (this.passwordLength > 20 || this.passwordLength < 0){
-        document.getElementById("password").textContent = "password cant be more than 20 chars";
-    }
-    this.includeLowercase = document.getElementById("lowerCase").checked;
-    this.includeUppercase = document.getElementById("upperCase").checked;
-    this.includeNumbers = document.getElementById("numbers").checked;
-    this.includeSymbols = document.getElementById("symbols").checked;
-    
-}   
-
-function generate(){
-    document.getElementById("copyMessage").value = "";
-    document.getElementById("copied").style.display = "none";
-    generatePassword(this.passwordLength, this.includeLowercase, this.includeUppercase, this.includeNumbers, this.includeSymbols);
+function done() {
+  passwordLength = parseInt(document.getElementById("length").value);
+  includeLowercase = document.getElementById("lowerCase").checked;
+  includeUppercase = document.getElementById("upperCase").checked;
+  includeNumbers = document.getElementById("numbers").checked;
+  includeSymbols = document.getElementById("symbols").checked;
 }
 
+function generate() {
+  document.getElementById("copyMessage").value = "";
+  document.getElementById("copied").style.display = "none";
+  generatePassword(
+    passwordLength,
+    includeLowercase,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  );
+}
 
-
-function show() {
+function toggleDivs() {
   const settingsDiv = document.getElementById("settings");
-  settingsDiv.style.display =
-    settingsDiv.style.display === "none" ? "block" : "none";
+  const containerDiv = document.getElementById("container");
+
+  if (settingsDiv.style.display === "none") {
+    settingsDiv.style.display = "flex";
+    containerDiv.style.display = "none";
+  } else {
+    settingsDiv.style.display = "none";
+    containerDiv.style.display = "flex";
+  }
 }
 
-function handleClick(){
-    show();
-    done();
+function handleClick() {
+  toggleDivs();
+  done();
 }
 
 function copyText() {
